b515d0cbb6626ccf0582b44e8043cdc4c07a271b
almarpa <almarpa4@gmail.com>
2020-10-23 11:11:17 +0200

    Initial commit

---
+++ spring-backend/src/test/java/com/alejandro/backendapi/SpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alejandro.backendapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
